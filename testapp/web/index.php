ffc9f9de6696d8af104db16a7f661bed4712868c
Erika Heidi <erika@erikaheidi.com>
2017-01-23 16:00:02 +0100

    vagrant push example

--- testapp/web/index.php
+++ testapp/web/index.php
@@ -2,7 +2,7 @@
 <p><img src="img/sc0<?php echo rand(1,4); ?>.gif" align="center" style="display:block;margin:0 auto;height:80%"></p>
 
 <h1>Your environment is UP and RUNNING!</h1>
-<h3>NGINX + PHP Version <?php echo PHP_VERSION ?></h3>
+<h3>PHP Version <?php echo PHP_VERSION ?></h3>
 <p><a href="info.php">check the phpinfo()</a>
 </div>
 
