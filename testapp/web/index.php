3e597a0a52f608447c0106e40dad47b8c67be1b5
Erika Heidi <erika@erikaheidi.com>
2017-01-23 12:32:36 +0100

    Updated Test APP

--- testapp/web/index.php
+++ testapp/web/index.php
@@ -1,4 +1,9 @@
-<h1>Welcome to Vagrant Cookbook</h1>
-<p>This is your environment</p>
-<?php
-phpinfo();
+<div style="text-align: center;margin: 0 auto">
+<p><img src="img/sc0<?php echo rand(1,4); ?>.gif" align="center" style="display:block;margin:0 auto;height:80%"></p>
+
+<h1>Your environment is UP and RUNNING!</h1>
+<h3>NGINX + PHP Version <?php echo PHP_VERSION ?></h3>
+<p><a href="info.php">check the phpinfo()</a>
+</div>
+
+
